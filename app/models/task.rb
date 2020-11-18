1762c9450f00ec96b1048ce28c064f8404915d28
egor <egor.odessa@gmail.com>
2020-11-18 02:48:27 +0200

    min func, sc validation, CRUD 2 models one page

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,6 @@ class Task < ApplicationRecord
 	belongs_to :project
 
 	validates :title, length: { in: 2..80 }, presence: true
-	validates_associated :project
+	validates :done, inclusion: { in: [true, false] }
+	validates_associated :project	
 end
